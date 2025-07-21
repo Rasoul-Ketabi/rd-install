9339865ed1549a33c2e0ef5b9ebfd3844e5ed2e2
Rasoul-Ketabi <rasoul@persisca.com>
2025-07-21 21:48:07 +0330

    ufw ports

--- rd-install.sh
+++ rd-install.sh
@@ -396,7 +396,7 @@ main() {
   post_custom	
 	
   bbb-conf --check
-  
+  bbb-conf --restart
   bbb-conf --secret
 }
 
@@ -406,6 +406,8 @@ open_ports_ufw(){
 	sudo ufw allow 80/tcp
 	sudo ufw allow 443/tcp
 	sudo ufw allow 7443/tcp
+	sudo ufw allow 8085/tcp
+	sudo ufw allow 8021/tcp
 	sudo ufw allow 16384:32768/udp
 
 	return 0;
@@ -424,6 +426,17 @@ post_custom(){
 	sed -i 's|^defaultLogoURL=.*|defaultLogoURL=https://getreddot.com/images/logo.png|' /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties
 	sed -i 's|^defaultDarkLogoURL=.*|defaultDarkLogoURL=https://getreddot.com/images/darklogo.png|' /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties
 	sed -i 's|^beans.presentationService.defaultUploadedPresentation=.*|beans.presentationService.defaultUploadedPresentation=https://getreddot.com/default.pdf|' /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties
+	echo "Applying Reddot brand replacements to HTML5 client..."
+	sed -i 's|"app.navBar.optionsDropdown.openAppLabel": "Open in BigBlueButton Tablet app"|"app.navBar.optionsDropdown.openAppLabel": "Open in Reddot Tablet app"|' /usr/share/bigbluebutton/html5-client/locales/en.json
+	sed -i 's|"app.about.version_label": "BigBlueButton version:"|"app.about.version_label": "Reddot version:"|' /usr/share/bigbluebutton/html5-client/locales/en.json
+	sed -i 's|"app.mobileAppModal.title": "Open BigBlueButton Tablet app"|"app.mobileAppModal.title": "Open Reddot Tablet app"|' /usr/share/bigbluebutton/html5-client/locales/en.json
+	sed -i 's|"app.mobileAppModal.description": "Do you have the BigBlueButton Tablet app installed on your device\?"|"app.mobileAppModal.description": "Do you have the Reddot Tablet app installed on your device\?"|' /usr/share/bigbluebutton/html5-client/locales/en.json
+	sed -i 's|"app.guest.windowTitle": "BigBlueButton - Guest Lobby"|"app.guest.windowTitle": "Reddot - Guest Lobby"|' /usr/share/bigbluebutton/html5-client/locales/en.json
+	sed -i "s|\"app.feedback.subtitle\": \"We'd love to hear about your experience with BigBlueButton (optional)\"|\"app.feedback.subtitle\": \"We'd love to hear about your experience with Reddot (optional)\"|" /usr/share/bigbluebutton/html5-client/locales/en.json
+	sed -i 's|"app.feedback.textarea": "How can we make BigBlueButton better\?"|"app.feedback.textarea": "How can we make Reddot better?"|' /usr/share/bigbluebutton/html5-client/locales/en.json
+	sed -i 's|"app.learningDashboard.bigbluebuttonTitle": "BigBlueButton"|"app.learningDashboard.bigbluebuttonTitle": "Reddot"|' /usr/share/bigbluebutton/html5-client/locales/en.json
+	sed -i 's|"mobileApp.portals.fields.name.placeholder": "BigBlueButton demo"|"mobileApp.portals.fields.name.placeholder": "Reddot demo"|' /usr/share/bigbluebutton/html5-client/locales/en.json
+
 	wget -O /var/www/bigbluebutton-default/logo.png https://getreddot.com/logo.png
 	wget -O /var/www/bigbluebutton-default/assets/logo.png https://getreddot.com/logo.png
 	wget -O /var/www/bigbluebutton-default/assets/images/logo.png https://getreddot.com/logo.png
